ea054eaf60e71f4c5960fbde87430eca1e34d8ec
Peer Sommerlund <peer.sommerlund@gmail.com>
2025-09-25 12:17:47 +0200

    feat: Repository independent print
    [ ] write print2.rs
    
    Most print (unicode / svg) can also be split into
    generic and backend-specific. 
    
    The generic part renders the graph.
    
    The backend specific part renders the commit info.

---
+++ src/print2.rs
@@ -0,0 +1,50 @@
+//! Create visual representations of git graphs.
+
+use crate::graph2::BranchGraph;
+use std::cmp::max;
+
+pub use crate::print::colors;
+/* TODO figure out which should be used
+pub mod format;
+pub mod svg;
+pub mod unicode;
+*/
+
+/* TODO reiimplementon graph2::BranchGraph, if needed. Otherwise KILL
+
+/// Find the index at which a between-branch connection
+/// has to deviate from the current branch's column.
+///
+/// Returns the last index on the current column.
+fn get_deviate_index(graph: &GitGraph, index: usize, par_index: usize) -> usize {
+    let info = &graph.commits[index];
+
+    let par_info = &graph.commits[par_index];
+    let par_branch = &graph.all_branches[par_info.branch_trace.unwrap()];
+
+    let mut min_split_idx = index;
+    for sibling_oid in &par_info.children {
+        if let Some(&sibling_index) = graph.indices.get(sibling_oid) {
+            if let Some(sibling) = graph.commits.get(sibling_index) {
+                if let Some(sibling_trace) = sibling.branch_trace {
+                    let sibling_branch = &graph.all_branches[sibling_trace];
+                    if sibling_oid != &info.oid
+                        && sibling_branch.visual.column == par_branch.visual.column
+                        && sibling_index > min_split_idx
+                    {
+                        min_split_idx = sibling_index;
+                    }
+                }
+            }
+        }
+    }
+
+    // TODO: in cases where no crossings occur, the rule for merge commits can also be applied to normal commits
+    // See also branch::trace_branch()
+    if info.is_merge {
+        max(index, min_split_idx)
+    } else {
+        (par_index as i32 - 1) as usize
+    }
+}
+*/
